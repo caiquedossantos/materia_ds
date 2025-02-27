0f6a7b0729f4be905061fff3c6ef9e4b853af697
caiquedossantos <151476904+caiquedossantos@users.noreply.github.com>
2025-02-27 23:05:18 +0000

    salvar

--- Estado.java
+++
@@ -1,7 +0,0 @@
-public enum Estado {
-    BAHIA("Bahia","BA"),
-    SAO_PAULO("São Paulo", "SP"),
-    RIO_DE_JANEIRO("Rio de Janeiro","RJ")
-}
-
-private String   8
--- atividade_ds_1/MainAtividadeDs.java
+++
@@ -1,7 +0,0 @@
-package atividade_ds_1;
-
-public class MainAtividadeDs {
-    public static void main(String[] args) {
-        System.out.println("Usando Branch...");
-    }
-}
